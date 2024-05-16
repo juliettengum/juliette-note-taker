4d66c68a5ce204c5335556c29ebb6f1a947c5e71
juliette maloke <your@email.com>
2024-05-15 22:17:33 -0500

    best work

--- server.js
+++ server.js
@@ -1,39 +1,38 @@
-// Dependencies
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
 const express = require("express");
-const path = require("path");
-
-// Sets up the express app
+const path = require('path');
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// Tells node that we are creating an "express" server
 const app = express();
-const PORT = process.env.PORT || 3001
-// Sets up the express app to handle data parsing
+
+// Sets an initial port. We"ll use this later in our listener
+const PORT = process.env.PORT || 3001;
+
+// MIDDLEWARE
+// Sets up the Express app to handle data parsing for POST and PUT requests, because in both these requests you are sending data to the server and you are asking the server to accept or store that data.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Middleware to set the correct MIME type for CSS files
-app.use((req, res, next) => {
-    if (req.path.endsWith('.css')) {
-      res.header('Content-Type', 'text/css');
-    }
-    next();
-  });
+// Sets up the Express app to serve static files
+app.use(express.static("public"));
 
-// Middleware to set the correct MIME type for JS files
-app.use((req, res, next) => {
-    if (req.path.endsWith('.js')) {
-      res.header('Content-Type', 'application/javascript');
-    }
-    next();
-  });
-  
 
-  app.use("/assets", express.static(path.join(__dirname, "public", "assets")));
-  
-
-//Routes
-require("./routes/html-routes")(app);
-require("./routes/api-routes")(app);
-
-// Starts the server to begin listening
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+// ROUTER
+// The below points our server to a series of "route" files.
+require("./routes/api.routes")(app);
+require("./routes/html.routes")(app);
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+
+// LISTENER
+app.listen(PORT, () =>
+  console.log(`Express server currently running on port: ${PORT}`)
+);
